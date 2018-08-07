466eca479c838df0dcdf013f3b48cc03ee88428d
kotaro-kawashiki <39474830+kotaro-kawashiki@users.noreply.github.com>
2018-08-07 16:55:48 +0900

    '20180806'

--- src/main/java/com/example/fabrikam/TodoDemo/TodoDemoController.java
+++ src/main/java/com/example/fabrikam/TodoDemo/TodoDemoController.java
@@ -3,9 +3,7 @@ package com.example.fabrikam.TodoDemo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 
@@ -32,13 +30,45 @@ public class TodoDemoController {
     }
 
     @RequestMapping("/update")
+
     public String updateTodo(@ModelAttribute TodoListViewModel requestItems) {
+        ArrayList<TodoItem> todoList = (ArrayList<TodoItem>) repository.findAll();
+        ArrayList<String> names = new ArrayList<>();
+
         for (TodoItem requestItem : requestItems.getTodoList() ) {
-             TodoItem item = new TodoItem(requestItem.getCategory(), requestItem.getName());
-             item.setComplete(requestItem.isComplete());
-             item.setId(requestItem.getId());
-             repository.save(item);
+            TodoItem item = new TodoItem(requestItem.getCategory(), requestItem.getName());
+            item.setComplete(requestItem.isComplete());
+            item.setId(requestItem.getId());
+            repository.save(item);
         }
+
+
+        for(TodoItem todoItem : todoList){
+            if(todoItem.isComplete()){
+                repository.delete(todoItem);
+            }
+            else{
+                names.add(todoItem.getName());
+            }
+        }
+
         return "redirect:/";
+
     }
+
+
+    @RequestMapping("/edit/{id}")
+//    @ResponseBody
+    public String editTodo(Model model, @PathVariable("id") long id) {
+        TodoItem todoItem = repository.findOne(id);
+        if(todoItem == null) {
+            model.addAttribute("test", "からっぽですよ！！！");
+            return "edit";
+        } else {
+            model.addAttribute("test", todoItem.getName());
+            return "edit";
+//        return "Get a specific Foo with id=" + id;
+        }
+    }
+
 }
